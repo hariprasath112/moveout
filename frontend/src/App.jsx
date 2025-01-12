e44a216219b4b1d82060c3f82d3f882700d8758b
hariprasath112 <hariprasath01123@gmail.com>
2025-01-12 14:44:41 -0500

    remove main.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
-function App() {
 
+function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route index element={<Home />} />
-
+          <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
   )
 }
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
 export default App
