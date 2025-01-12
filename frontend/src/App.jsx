0fbe43cc51b3e2cddc2601114cfffdb390202c92
hariprasath112 <hariprasath01123@gmail.com>
2025-01-12 15:57:53 -0500

    login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.jsx';
+import Login from './pages/Login.jsx';
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />} />
+          <Route index element={<Login />} />
+          <Route path="/login" element={<Home />} />
           <Route path="/home" element={<Home />} />
         </Routes>
       </BrowserRouter>
