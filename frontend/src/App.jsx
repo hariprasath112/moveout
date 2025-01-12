f20de698fd168cf998db5588a04117e32443e791
hariprasath112 <hariprasath01123@gmail.com>
2025-01-11 23:18:50 -0500

    react dom routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,15 @@
-import { useState } from 'react'
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home.jsx';
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />} />
+
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
