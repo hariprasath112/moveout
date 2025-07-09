9660e9d81183147ab5c903cccf62711d4df3d353
hariprasath112 <hariprasath01123@gmail.com>
2025-07-09 17:25:37 -0400

    backend fixes

--- backend/index.js
+++ backend/index.js
@@ -46,7 +46,7 @@ app.get('/users', requireAuth,async (req, res) => {
 app.get('/script', requireAuth, async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM script');
-    if (!result[0]) return res.status(404).json({ error: 'No script found' })
+    //if (!result[0]) return res.status(404).json({ error: 'No script found' })
     res.json(result[0]);
   } catch (err) {
     console.error(err);
